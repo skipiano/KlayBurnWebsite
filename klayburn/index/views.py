a37bd2d76517eca81af38d7edf64356a51097402
Michael Hahn <skipiano@gmail.com>
2022-05-31 11:28:39 -0700

    figure out how to update graphs

--- klayburn/index/views.py
+++ klayburn/index/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Member
 from django.views import generic
+import matplotlib
+import matplotlib.pyplot as plt
+import io
+import base64
+matplotlib.use('Agg')
 
 
 class BlockView(generic.ListView):
@@ -17,19 +22,39 @@ def index(request):
 
 def block_member(request, pk):
     try:
+        context = {}
         member = Member.objects.get(pk=pk)
+        # debug
+        plt.plot([0, 1, 2, 3, 4], [0, 3, 5, 9, 11])
+        plt.xlabel('Months')
+        plt.ylabel('Books Read')
+        flike = io.BytesIO()
+        plt.savefig(flike)
+        b64 = base64.b64encode(flike.getvalue()).decode()
+        context['chart_all'] = b64
+        plt.close()
 
     except Member.DoesNotExist:
         raise Http404('Member does not exist')
 
-    return render(request, 'index/graph.html')
+    return render(request, 'index/graph.html', context=context)
 
 
 def transaction(request):
+    # debug
+    context = {}
+    plt.plot([0, 1, 2, 3, 4], [11, 9, 5, 3, 0])
+    plt.xlabel('Months')
+    plt.ylabel('Books Read')
+    flike = io.BytesIO()
+    plt.savefig(flike)
+    b64 = base64.b64encode(flike.getvalue()).decode()
+    context['chart_all'] = b64
+    plt.close()
 
-    return render(request, 'index/graph.html')
+    return render(request, 'index/graph_csv.html', context=context)
 
 
 def gas_fee(request):
 
-    return render(request, 'index/graph.html')
+    return render(request, 'index/graph_csv.html')
